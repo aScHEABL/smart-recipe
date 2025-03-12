3015437efc04d7b4c2c07119715c9877b34bcd5a
Zegoithaas Nack <22256864+aScHEABL@users.noreply.github.com>
2025-03-12 17:09:07 +0800

    wip: recipe card layout #0

--- smart_recipe/smart_recipe.py
+++ smart_recipe/smart_recipe.py
@@ -5,13 +5,18 @@ from rxconfig import config
 
 
 class State(rx.State):
+    # 儲存這些變數在Component state裡面
     query: str = ""
-    meals: list = []
+    meals: list[dict] = []
+    render_menu: bool = False
 
     async def search(self, query: str):
         self.query = query
+
+        # 如果欄位為空，停止渲染搜尋結果
         if not query:
             self.meals = []
+            self.render_menu = False
             print("User clears query")
             return
 
@@ -20,9 +25,31 @@ class State(rx.State):
             if response.status_code == 200:
                 data = response.json()
                 self.meals = data.get("meals", [])
-                print(self.meals)
+                self.render_menu = True
             else:
+                # 如果搜尋不到，停止宣染搜尋結果
                 self.meals = []
+                self.render_menu = False
+
+def ReusableCard(item: rx.Var):
+    return rx.cond(
+                State.render_menu,
+                rx.fragment(
+                    rx.flex(
+                        rx.card(
+                            rx.link(
+                                rx.image(src=item.strMealThumb, width="200px"),
+                                href=item.strYoutube.to(str),
+                                is_external=True,
+                            ),
+                        ),
+                        rx.heading(item.strMeal),
+                        wrap="wrap",
+                    )
+                ), # 條件式宣染，故意留白
+            )
+
+                
 
 def index() -> rx.Component:
     # 首頁 (Index)
@@ -33,8 +60,13 @@ def index() -> rx.Component:
                             type="search",
                             size="3",
                             width="400px",
-                            on_change=State.search
+                            on_change=State.search,
                         ),
+                rx.flex(rx.foreach(State.meals, ReusableCard),
+                            wrap="wrap",
+                            justify="center",
+                        ),
+                wrap="wrap",
             )
     )
 
